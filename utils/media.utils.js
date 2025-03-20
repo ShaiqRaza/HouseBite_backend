fc483111271ca767c615f7732717ec571f1cbadd
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-21 02:55:36 +0500

    add images upload/deletion utils

---
+++ utils/media.utils.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+export const imageUpload = async (filePath) => {
+    try {
+        if(!filePath) 
+            throw error("No file provided to upload!");
+        return await cloudinary.uploader.upload(filePath);
+    } catch (error) {
+        throw error("An error occurred while uploading image to cloudinary:", error);
+    }
+};
+
+export const imageDelete = async (public_id) => {
+    try {
+        if(!public_id) 
+            throw error("No public_id provided to delete image!");
+        return await cloudinary.uploader.destroy(public_id);
+    } catch (error) {
+        throw error("An error occurred while deleting image from cloudinary:", error);
+    }
+};
