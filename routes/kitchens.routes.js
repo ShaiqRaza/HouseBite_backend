5d64729f95ec130bf525358bebda4328b35a886a
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-22 16:46:56 +0500

    add getRunningSubscriptions controller

--- routes/kitchens.routes.js
+++ routes/kitchens.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllkitchens, createKitchen, deleteKitchen, updateKitchenDetails, updateKitchenImage } from '../controllers/kitchens.controllers.js';
+import { getAllkitchens, createKitchen, deleteKitchen, updateKitchenDetails, updateKitchenImage, getRunningSubscriptions } from '../controllers/kitchens.controllers.js';
 import upload from '../configurations/multer.config.js';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post('/create', upload.single('image'), createKitchen);
 router.delete('/:id', deleteKitchen);
 router.post('/update/details/:id', updateKitchenDetails);   
 router.post('/update/image/:id', upload.single('image'), updateKitchenImage);
+router.get('/subscriptions/running/:id', getRunningSubscriptions)
 
 export default router;
