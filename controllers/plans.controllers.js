5c0972c582493e437baf145eafc6db5a72efd111
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-23 16:14:52 +0500

    add delete plan  controller

--- controllers/plans.controllers.js
+++ controllers/plans.controllers.js
@@ -42,3 +42,76 @@ export const updatePlan = async (req, res) => {
         });
     }
 };
+
+export const deleteMeal = async (req, res) => {
+    const id = req.params.id;
+    if (!id) 
+        return res.status(400).json({ message: "Meal ID is required." });
+
+    try{
+        await sql.query`DELETE FROM meals WHERE id=${id}`;
+        res.status(200).json({ message: "Meal deleted successfully." });
+    }
+    catch(err){
+        res.status(500).json({ 
+            message: "An error occurred while deleting the meal.",
+            error: err.message 
+        });
+    }
+};
+
+export const updateMeal = async (req, res) => {
+    const {name, price} = req.body || {};
+    const id = req.params.id;
+
+    if(!id)//meal id is required
+        return res.status(400).json({ message: "Meal ID is required." });
+
+    if(!(name || price)) 
+        return res.status(400).json({ message: "Nothing to update." });
+
+    let updates = [];
+    if(name) updates.push(`name = '${name}'`);
+    if(price) updates.push(`price = ${price}`);
+
+    try{
+        const meal = await sql.query(`UPDATE meals SET ${updates.join(", ")} output inserted.* WHERE id=${id}`);
+        res.status(200).json(meal.recordset[0]);
+    }
+    catch(err){
+        res.status(500).json({ 
+            message: "An error occurred while updating the meal.",
+            error: err.message 
+        });
+    }
+};
+
+export const addMeal = async (req, res) => {
+    const id = req.params.id;
+    if(!id)
+        return res.status(400).json({ message: "Plan ID is required." });
+
+    const {name, price, meal_days} = req.body || {};
+    if(!name || !price || meal_days.length===0)
+        return res.status(400).json({ message: "All fields are required." });
+
+    let meal = null;
+    
+    try{
+        meal = await sql.query(`INSERT INTO meals (name, price, plan_id) output inserted.* VALUES (${name}, ${price}, ${id})`);
+        const meal_id = meal.recordset[0].id;
+        const meal_days_insert = await Promise.all(
+            meal_days.map(async (meal_day)=>{
+                return await sql.query(`INSERT INTO meal_days (meal_id, day, timing) VALUES (${meal_id}, ${meal_day.day}, ${meal_day.timing})`);
+            })
+        )
+    }
+    catch(err){
+        if(meal)
+            await sql.query(`DELETE FROM meals WHERE id=${meal.recordset[0].id}`);
+        res.status(500).json({ 
+            message: "An error occurred while adding the meal.",
+            error: err.message 
+        });
+    }
+};
--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -1,9 +1,10 @@
-import { deletePlan, updatePlan } from "../controllers/plans.controllers.js";
+import { deletePlan, updatePlan, deleteMeal } from "../controllers/plans.controllers.js";
 import express from 'express';
 
 const router = express.Router();
 
 router.delete('/delete/:id', deletePlan);
 router.post('/update/:id', updatePlan);
+router.delete('/delete/meal/:id', deleteMeal);//meal id as id
 
 export default router;
