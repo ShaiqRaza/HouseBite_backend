22c9d4d2e96692a4c2c272e4e06d3ad2baf5ea3f
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-23 10:06:21 +0500

    add getAllsubscriptions controller

--- controllers/subscriptions.controllers.js
+++ controllers/subscriptions.controllers.js
@@ -1,4 +1,16 @@
-import sql from 'sql';
+import sql from '../configurations/db.config.js';
+
+export const getAllSubscriptions = async (req, res) => {
+    try {
+        const subscriptions = await sql.query('SELECT * FROM subscriptions');
+        res.status(200).json(subscriptions.recordset);
+    } catch (error) {
+        res.status(500).json({
+            message: "An error occurred while fetching subscriptions.",
+            error: error.message
+        });
+    }
+};
 
 export const getPlanForSubscription = async (req, res) => {
     const id = req.params.id;
--- routes/subscriptions.routes.js
+++ routes/subscriptions.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getPlanForSubscription } from '../controllers/subscriptions.controllers.js';
+import { getPlanForSubscription, getAllSubscriptions } from '../controllers/subscriptions.controllers.js';
 
 const router = express.Router();
 
 router.get('/plan/:id', getPlanForSubscription);
+router.get('/', getAllSubscriptions);
 
 export default router;
