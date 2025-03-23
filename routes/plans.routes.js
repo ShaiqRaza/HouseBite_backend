92f794210012851905584158ad9e91dafe83e198
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-23 16:16:04 +0500

    add add meal controller

--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -1,4 +1,4 @@
-import { deletePlan, updatePlan, deleteMeal, updateMeal } from "../controllers/plans.controllers.js";
+import { deletePlan, updatePlan, deleteMeal, updateMeal, addMeal } from "../controllers/plans.controllers.js";
 import express from 'express';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ router.delete('/delete/:id', deletePlan);
 router.post('/update/:id', updatePlan);
 router.delete('/delete/meal/:id', deleteMeal);//meal id as id
 router.post('/update/meal/:id', updateMeal);//meal id as id
+router.post('/add/meal/:id', addMeal);//plan id as id
 
 export default router;
