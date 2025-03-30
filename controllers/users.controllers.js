6e9cb4e78331f7955275189a888b5c13cd450280
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-30 11:40:01 +0500

    add update user controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -48,6 +48,48 @@ export const createUser = async (req, res) => {
     }
 }
 
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const {name, phone, address, latitude, longitude, password} = req.body || {};
+
+    if (!id) {
+        return res.status(400).json({ message: "User ID is required for updating." });
+    }
+
+    if (!name && !phone && !address && !latitude && !longitude && !password) {
+        return res.status(400).json({ message: "Nothing to update." });
+    }
+
+    try{
+        const kitchen = await sql.query`SELECT * FROM Users WHERE id=${id}`;
+
+        if(kitchen.recordset.length == 0){
+            return res.status(404).json({ message: "User ID is incorrect!" });
+        }
+
+        const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
+
+        let updates = [];
+        if(name) updates.push(`name='${name}'`);//here we are building the query manually so have to write value of sting within single quotes because sql sriver will not do this automatically
+        if(phone) updates.push(`phone='${phone}'`);   
+        if(address) updates.push(`address='${address}'`);
+        if(latitude) updates.push(`latitude=${latitude}`);
+        if(longitude) updates.push(`longitude=${longitude}`);
+        if(hashedPassword) updates.push(`password='${hashedPassword}'`);
+
+        const query = `UPDATE Users SET ${updates.join(", ")} output inserted.* WHERE id = ${id};`;
+        const updatedUser = await sql.query(query);
+
+        res.status(200).json(updatedUser.recordset[0]);
+    }
+    catch(err){
+        res.status(500).json({ 
+            message: "An error occurred while updating the user.",
+            error: err.message 
+        });
+    }
+}
+
 export const subscribePlan = async (req, res) => {
     const {user_id, plan_id} = req.params || {};
     if(!user_id)
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,11 @@
-import { getAllUsers, createUser, subscribePlan } from "../controllers/users.controllers.js";
+import { getAllUsers, createUser, subscribePlan, updateUser } from "../controllers/users.controllers.js";
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.post('/create', createUser);
+router.post('/update/:id', updateUser);
 router.post('/subscribe/:user_id/:plan_id', subscribePlan);
 
 export default router;
