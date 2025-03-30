01822ba5eeac52246ca8f6c20fbbafc01293dbaf
ShaiqRaza <shaiqraza808y@gmail.com>
2025-03-30 14:02:34 +0500

    setup reply controller

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -1,4 +1,4 @@
-import {addReview, editReview, deleteReview} from '../controllers/reviews.controllers.js';
+import {addReview, editReview, deleteReview, replyToReview, editReply} from '../controllers/reviews.controllers.js';
 import express from 'express';
 
 const router = express.Router();
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/add/:user_id/:kitchen_id', addReview);
 router.post('/edit/:user_id/:review_id', editReview);
 router.delete('/delete/:id', deleteReview);
+router.post('/reply/:kitchen_id/:review_id', replyToReview);
+router.post('/reply/edit/:id', editReply);
 
 export default router;
